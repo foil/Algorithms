1107accbbbe2725f363fac873d481fd5593a6ed3
Matt Zhang <mattz@vrt.com.au>
2019-09-03 17:24:29 +1000

    buggy

--- src/com/pillow/InsertInterval.java
+++ src/com/pillow/InsertInterval.java
@@ -1,5 +1,8 @@
 package com.pillow;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
 /*
    Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
 
@@ -20,7 +23,8 @@ class InsertInterval {
         List<int[]> res = new ArrayList<>();
         if (intervals.length == 0) {
             res.add(newInterval);
-        } else {
+            return res.toArray(new int[0][0]);
+        }
         int[] range = find(intervals, newInterval);
         if (range[0] == -1) {
             if (range[1] == intervals.length)
@@ -30,15 +34,46 @@ class InsertInterval {
                 tmp[0] = newInterval[0];
                 tmp[1] = intervals[range[1]][1];
                 res.add(tmp);
-                //  add rest of intervals
+                for (int i = range[1] + 1; i < intervals.length; i ++)
+                    res.add(intervals[i]);
             }
+        } else if (range[1] == range[0]) {
+            return intervals;
+        } else if (newInterval[1] < intervals[range[1]][0] && newInterval[0] > intervals[range[0]][1]) {
+            for (int i = 0; i <= range[0]; i ++)
+                res.add(intervals[i]);
+            res.add(newInterval);
+            for (int i = range[1]; i < intervals.length; i ++)
+                res.add(intervals[i]);
+        } else if (newInterval[0] > intervals[range[0]][1]) {
+            for (int i = 0; i <= range[0]; i ++)
+                res.add(intervals[i]);
+            int[] tmp = new int[2];
+            tmp[0] = newInterval[0];
+            tmp[1] = intervals[range[1]][1];
+            res.add(tmp);
+            for (int i = range[1] + 1; i < intervals.length; i ++)
+                res.add(intervals[i]);
+        } else if (newInterval[1] < intervals[range[1]][0]) {
+            for (int i = 0; i < range[0]; i ++)
+                res.add(intervals[i]);
+            int[] tmp = new int[2];
+            tmp[0] = intervals[range[0]][0];
+            tmp[1] = newInterval[1];
+            res.add(tmp);
+            for (int i = range[1]; i < intervals.length; i ++)
+                res.add(intervals[i]);
         } else {
-            if (range[1] == range[0]) {
-                return intervals;
-            }
+            for (int i = 0; i < range[0]; i ++)
+                res.add(intervals[i]);
+            int[] tmp = new int[2];
+            tmp[0] = intervals[range[0]][0];
+            tmp[1] = intervals[range[1]][1];
+            res.add(tmp);
+            for (int i = range[1] + 1; i < intervals.length; i ++)
+                res.add(intervals[i]);
         }
-        }
-        return res.toArray();
+        return res.toArray(new int[0][0]);
     }
 
     private int[] find(int[][] intervals, int[] newInterval) {
@@ -61,5 +96,17 @@ class InsertInterval {
         }
         res[1] = i;
         return res;
+    }
+
+    public static void main(String[] args) {
+        InsertInterval s = new InsertInterval();
+        //int[][] intervals = new int[][] {{1,3},{6,9}}; int[] newInterval = new int[] {2,5};
+        //Output: [[1,5],[6,9]]
+        //int[][] intervals = new int[][] {{1,2},{3,5},{6,7},{8,10},{12,16}}; int[] newInterval = new int[] {4,8};
+        //Output: [[1,2],[3,10],[12,16]]
+        int[][] intervals = new int[][] {{1,5}}; int[] newInterval = new int[] {2,7};
+        //Output: [[1,7]]
+        int[][] res = s.insert(intervals, newInterval);
+        System.out.println(Arrays.deepToString(res));
     }
 }
